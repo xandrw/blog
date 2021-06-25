c683b62c9378153aa3cbfb08a6ded492d9fe95eb
Alexandru Cosor <alexandru@Alexandrus-MacBook-Pro.local>
2021-06-25 18:49:32 +0300

    Added roles migrations

---
+++ app/Admin/Roles/AdminRolesServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Admin\Roles;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminRolesServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+}
