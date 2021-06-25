07ac1ab66fdc623a9267f0d9368a8a8b4304a45a
Alexandru Cosor <alexandru@Alexandrus-MacBook-Pro.local>
2021-06-25 18:20:37 +0300

    Changed registration validation from alpha to string

--- app/Auth/Http/Requests/RegisterRequest.php
+++ app/Auth/Http/Requests/RegisterRequest.php
@@ -9,7 +9,7 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'alpha', 'min:5', 'max:255'],
+            'name' => ['required', 'string', 'min:5', 'max:255'],
             'email' => 'required|string|email|max:255|unique:users',
             'password' => ['required', 'confirmed', 'min:8']
         ];
